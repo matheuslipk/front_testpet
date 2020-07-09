d88f73e37dc2c81fa5a2891dcd22c2877156ce94
Matheus Araujo <matheusnumb1000@gmail.com>
2020-07-08 23:14:43 -0300

    .

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import {baseUrl} from '../helpers/constants'
 
 const api = axios.create({
-  baseURL: baseUrl.local,
+  baseURL: baseUrl.external,
 })
 
 export default api
