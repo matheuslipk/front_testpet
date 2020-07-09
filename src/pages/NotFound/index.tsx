65fd745d40a9348a541960d706aa7295d49ec409
Matheus Araujo <matheusnumb1000@gmail.com>
2020-07-08 21:39:35 -0300

    Redirecionando para login

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import {Redirect} from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <h1>
-      <Link to="sign_in">Login</Link>    
-    </h1>
+    <Redirect to="sign_in" />
   )
 }
 
